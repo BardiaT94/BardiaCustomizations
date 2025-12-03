f743414a49ab3aaba3c50f7a435bdffd0837b630
Bardia <bardia.taghavi1994@gmail.com>
2025-12-03 17:50:51 -0500

    Restore fixed

--- restore.sh
+++ restore.sh
@@ -7,12 +7,22 @@
 
 echo "🚀 Starting restore..."
 
-echo "Restoring Hyprland and Waybar customizations..."
+echo "Restoring Hyprland customizations..."
 cp -f "$PWD/Hyprland/userprefs.conf" "$HOME/.config/hypr/"
-cp -f "$PWD/Hyprland/BardiaWaybar.jsonc" "$HOME/.local/share/waybar/layouts/hyprdots/"
 cp -f "$PWD/Hyprland/hypridle.conf" "$HOME/.config/hypr/"
+cp -f "$PWD/Hyprland/hyprlock.conf" "$HOME/.config/hypr/"
 cp -f "$PWD/Hyprland/monitors.conf" "$HOME/.config/hypr/"
-cp -f "$PWD/Hyprland/user-style.css" "$HOME/.config/waybar/"
+
+echo "Restoring Waybar customizations..."
+cp -f "$PWD/Hyprland/BardiaWaybar.jsonc" "$HOME/.local/share/waybar/layouts/hyprdots/"
+cp -f "$PWD/Hyprland/BardiaWaybar.css" "$HOME/.local/share/waybar/styles/"
+cp -f "$PWD/Hyprland/spotify.xml" "$HOME/.local/share/waybar/menus/"
+cp -f "$PWD/Hyprland/custom-workflows.jsonc" "$HOME/.local/share/waybar/modules/"
+cp -f "$PWD/Hyprland/group-volumecontrol.jsonc" "$HOME/.local/share/waybar/modules/"
+
+echo "Restoring CPUinfo.sh and GPUinfo.sh..."
+cp -f "$PWD/Hyprland/cpuinfo.sh" "$HOME/.local/lib/hyde/"
+cp -f "$PWD/Hyprland/gpuinfo.sh" "$HOME/.local/lib/hyde/"
 
 echo "Restoring Fonts folder..."
 cp -rf "$PWD/Fonts/." "$HOME/.fonts"
@@ -23,21 +33,15 @@ cp -rf "$PWD/InkScape/palettes" "$HOME/.config/inkscape/"
 echo "Restoring Kitty customizations..."
 cp -rf "$PWD/Kitty/." "$HOME/.config/kitty"
 
+echo "Restoring MPV customizations..."
+cp -rf "$PWD/MPV/." "$HOME/.config/mpv"
+
 echo "Restoring Xilinx themes and waveform..."
 cp -rf "$PWD/Xilinx/themes/" "$HOME/.Xilinx/Vivado/2025.1"
 cp -rf "$PWD/Xilinx/waveform/" "$HOME/.Xilinx/Vivado/2025.1"
 
-echo "Restoring GTK themes..."
-cp -rf "$PWD/GTK/.themes" "$HOME"
-
 echo "Restoring Thunar customizations..."
 cp -rf "$PWD/Thunar" "$HOME/.config"
-
-echo "Restoring KDE customizations..."
-cp -f "$PWD/KDE/kdeglobals" "$HOME/.config/kdeglobals"
-
-echo "Restoring Spicetify customizations..."
-cp -rf "$PWD/Spicetify/." "$HOME/.config/spicetify"
 
 echo ""
 echo "✅ Restore complete! All files have been copied back from $PWD"
